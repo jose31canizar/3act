5a8bb2261fa494156596c3cf1bb5d7c9b7224525
Prayash Thapa <prayasht@gmail.com>
2017-01-31 11:55:58 -0700

    Clean up and add some more anim

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,28 +6,18 @@ var OrbitControls = require('three-orbit-controls')(THREE);
 import Sky from './Sky';
 import Box from './Box';
 
+const perspectiveCameraRotation = new THREE.Euler(0, Math.PI, 0);
+
 class Application extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.fog = new THREE.Fog(0xCCE0FF, 500, 10000);
-    this.cameraPosition = new THREE.Vector3(0, 40, 120);
-
-    // Construct the position vector here, because if we use 'new' within render,
-    // React will think that things have changed when they have not.
 
     this.state = {
-      cubeRotation: new THREE.Euler()
-    };
-
-    this._onAnimate = () => {
-      // we will get this callback every frame
-
-      // pretend cubeRotation is immutable.
-      // this helps with updates and pure rendering.
-      // React will be sure that the rotation has now updated.
-      this.setState({
-        cubeRotation: new THREE.Euler(this.state.cubeRotation.x + 0.01, this.state.cubeRotation.y + 0.01, 0)
-      });
+      ... this.state,
+      r: Date.now() * 0.0005,
+      cubeRotation: new THREE.Euler(),
+      mainCameraPosition: new THREE.Vector3(0, 40, 120)
     };
   }
 
@@ -41,9 +31,20 @@ class Application extends React.Component {
     delete this.controls;
   }
 
+  _onAnimate = () => {
+    const r = Date.now() * 0.0005;
+
+    this.setState({
+      r,
+      cubeRotation: new THREE.Euler(this.state.cubeRotation.x + 0.01, this.state.cubeRotation.y + 0.01, 0)
+    });
+  };
+
   render() {
     var width = window.innerWidth;
     var height = window.innerHeight;
+
+    const { r } = this.state;
 
     var rendererProps = {
       antialias: 1,
@@ -56,11 +57,12 @@ class Application extends React.Component {
     };
 
     var cameraProps = {
-      fov: 75,
+      fov: 35 + 30 * Math.sin(0.5 * r),
       aspect: (width / height),
       near: 0.1,
       far: 10000,
-      position: this.cameraPosition
+      position: this.state.mainCameraPosition
+      // rotation: perspectiveCameraRotation
     };
 
     return (
